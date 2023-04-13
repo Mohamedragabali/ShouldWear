12102fd7c8a7966f0b820b001e25ac777ea83de7
Mohamed Ragab <mohamed.ragab.ali.9@gmail.com>
2023-04-13 17:45:44 +0200

    feat: add code to get location

--- app/src/main/java/com/mohamedragab/shouldwear/MainActivity.kt
+++ app/src/main/java/com/mohamedragab/shouldwear/MainActivity.kt
@@ -1,7 +1,18 @@
 package com.mohamedragab.shouldwear
 
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.gson.Gson
 import com.mohamedragab.shouldwear.data.WeatherResponse
 import com.mohamedragab.shouldwear.databinding.ActivityMainBinding
@@ -11,17 +22,26 @@ import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityMainBinding
     private val logInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
     val client = OkHttpClient.Builder().addInterceptor(logInterceptor).build()
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    var latitude:Double?=null
+    var longitude:Double?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        makeRequestOkHttp()
+
+        // makeRequestOkHttp()
+
+//        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+//        getCurrentLocation()
+
     }
 
     private fun makeRequestOkHttp() {
@@ -54,4 +74,90 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    private fun getCurrentLocation() {
+        if (checkPermission()) {
+            if (isLocationEnabled()) {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ) !=
+                    PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this, android.Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) !=
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermissions()
+                    return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    if (location == null) {
+                        Toast.makeText(this, "Null Recived", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Get Success", Toast.LENGTH_SHORT).show()
+                        latitude =location.latitude
+                        longitude =location.longitude
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Turn on location ", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            PERMISSION_PEQEST_ACCESS_LOCATION
+        )
+    }
+
+    companion object {
+        private const val PERMISSION_PEQEST_ACCESS_LOCATION = 100
+    }
+
+    private fun checkPermission(): Boolean {
+        if (
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(applicationContext, "Granted", Toast.LENGTH_SHORT).show()
+            getCurrentLocation()
+        } else {
+            Toast.makeText(applicationContext, "Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
